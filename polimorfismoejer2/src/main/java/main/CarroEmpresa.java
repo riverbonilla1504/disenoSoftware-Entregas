e79b12f0db5d0c59fc08a5048320a20d36e5fbe7
river PC <river.bonilla@campusucc.edu.co>
2024-10-13 14:57:41 -0500

    first commit

--- polimorfismoejer2/src/main/java/main/CarroEmpresa.java
+++ polimorfismoejer2/src/main/java/main/CarroEmpresa.java
@@ -5,7 +5,6 @@ public class CarroEmpresa {
     private String matricula;
     private String marca;
     private String modelo;
-    private static CarroEmpresa instance;
 
 
     public CarroEmpresa(String matricula, String marca, String modelo) {
